ff57432af4b8e062fd9bf4a302ad3530ca39f382
Rens van Woudenberg <rensvanw@outlook.com>
2024-06-27 09:37:29 +0200

    fixed polyfill exploit

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -7,7 +7,7 @@ var config = {
     map: {
         '*': {
             quicklink: 'https://cdnjs.cloudflare.com/ajax/libs/quicklink/2.0.0/quicklink.umd.js',
-            polyfillio: 'https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver'
+            polyfillio: 'https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=IntersectionObserver'
         }
     }
 };
